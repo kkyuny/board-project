d8195a76d0cf56471fa5b24e228fa5013e4bf301
kyun <kyun9911@gmail.com>
2025-09-05 11:57:00 +0900

    Ch03. 세부기능개발) 3~5번 강의 commit - AuditingFields 추상화 클래스로 수정  - 역할은 바뀌지 않지만 코드레벨에서 의도를 더 명확하게 표현

--- src/main/java/com/fastcampus/board_project/domain/AuditingFields.java
+++ src/main/java/com/fastcampus/board_project/domain/AuditingFields.java
@@ -18,22 +18,26 @@ import java.time.LocalDateTime;
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class AuditingFields {
+public abstract class AuditingFields {
+
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    protected LocalDateTime createdAt; // 생성일시
 
     @CreatedBy
     @Column(nullable = false, updatable = false, length = 100)
-    private String createdBy;
+    protected String createdBy; // 생성자
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime modifiedAt;
+    protected LocalDateTime modifiedAt; // 수정일시
 
     @LastModifiedBy
-    @Column(nullable = false)
-    private String modifiedBy;
+    @Column(nullable = false, length = 100)
+    protected String modifiedBy; // 수정자
+
 }
+
+
