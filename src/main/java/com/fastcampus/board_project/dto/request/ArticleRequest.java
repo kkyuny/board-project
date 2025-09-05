237e5ab5f5a9ff154a5579e2d23117900293b6d1
kyun <kyun9911@gmail.com>
2025-09-05 11:59:53 +0900

    Ch03. 세부기능개발) 3~5번 강의 commit - Request, Response 클래스 추가  - Request: Request 객체를 dto 객체로 변환  - Response: dto 객체를 Response 객체로 변환

---
+++ src/main/java/com/fastcampus/board_project/dto/request/ArticleRequest.java
@@ -0,0 +1,31 @@
+package com.fastcampus.board_project.dto.request;
+
+import com.fastcampus.board_project.dto.ArticleDto;
+import com.fastcampus.board_project.dto.HashtagDto;
+import com.fastcampus.board_project.dto.UserAccountDto;
+
+import java.util.Set;
+
+public record ArticleRequest(
+        String title,
+        String content
+) {
+
+    public static ArticleRequest of(String title, String content) {
+        return new ArticleRequest(title, content);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return toDto(userAccountDto, null);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtagDtos
+        );
+    }
+
+}
