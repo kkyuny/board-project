5d1d2f79bd1114ca6410af789f70ced3dfcd4ef8
kyun <kyun9911@gmail.com>
2025-09-09 11:47:15 +0900

    Ch03. 세부기능개발) 9~10번 강의 commit - 게시글 컨트롤러 -> 페이징 정보 및 테스트 코드 추가 - 컨트롤러 테스트는 @WebMvcTest로 진행한다. - @WebMvcTest는 서비스나 레포지토리는 로드하지 않고 컨트롤러 관련 객체만 로드하게 하게 되어 @Autowired로 접근할 수 있다. - 이 때 서비스 클래스를 사용하기 위하여 @MockitoBean을 이용하여 의존성을 주입한다. - 그리고 강의 코드는 @MockBean을 사용하였지만 스프링부트 3.4 버전 이후 성능문제로 데프리케이트 되어 @MockitoBean으로 사용이 대체 되었다고 함. 그래서 코드 변경 관련되어 코드 에러 수정 필요함.

--- src/test/java/com/fastcampus/board_project/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/board_project/controller/ArticleControllerTest.java
@@ -1,15 +1,33 @@
 package com.fastcampus.board_project.controller;
 
 import com.fastcampus.board_project.config.SecurityConfig;
+import com.fastcampus.board_project.service.ArticleService;
+import com.fastcampus.board_project.service.PaginationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,21 +38,35 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockitoBean
+    private ArticleService articleService;
+    @MockitoBean
+    private PaginationService paginationService;
+
+    @Autowired
+    private ArticleController controller;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-    @DisplayName("[view][GET] 게시글 리스트(게시판) 페이지 - 정상 호출")
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
-    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+    void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
+        given(articleService.searchArticles(eq(null), eq(null), (Pageable) any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
 
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("paginationBarNumbers"))
+                .andExpect(model().attributeExists("searchTypes"));
+        then(articleService).should().searchArticles(eq(null), eq(null), (Pageable) any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
